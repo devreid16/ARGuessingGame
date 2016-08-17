64a01743a756e4e2c126ab7590c22a2228bec101
Angela Reid <reidang94@gmail.com>
2016-08-17 13:29:22 -0400

    add style, chg text and links

---
+++ ArGuessingGame/Controllers/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ArGuessingGame.Models;
+
+namespace ArGuessingGame.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        public ActionResult Index()
+        {
+            Session["Answer"] = new Random().Next(1, 50);
+            return View();
+        }
+
+        private bool GuessIsCorrect(int guess)
+        {
+            return guess == (int)Session["Answer"];
+        }
+
+       [HttpPost]
+       [ValidateAntiForgeryToken]
+
+        public ActionResult Index(AngieGame model)
+        {
+           if (ModelState.IsValid)
+            {
+                ViewBag.Win = GuessIsCorrect(model.Guess);
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ ArGuessingGame/Models/AngieGame.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ArGuessingGame.Models
+{
+
+    public class AngieGame
+    {
+
+        [Display(Name = "Enter Your Name")]
+        [Required(ErrorMessage ="You Must Enter Your Name")]
+        [MinLength(2,ErrorMessage ="Name is too short.")]
+        [MaxLength(20,ErrorMessage ="Name is too long.")]
+        
+
+        public string PlayerName { get; set; }
+
+
+        [Display(Name = "Enter Your Guess")]
+        [Required(ErrorMessage ="You Must Enter a Guess")]
+        [Range(1, 50, ErrorMessage = "Number Must be between 1 and 50.")]
+
+        public int Guess { get; set; }
+
+        public override string ToString()
+        {
+            return $"{PlayerName} ({Guess})";
+        }
+    }    
+}
